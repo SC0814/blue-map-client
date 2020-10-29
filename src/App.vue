e2189f54dc435dafb30fcf1588d847721d78cf68
cuimingjia <cuimingjia@zhenrongbao.com>
2020-10-29 11:03:10 +0800

    git commit -m "first commit"

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">主要按钮</van-button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import { Button } from "vant";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    [Button.name]:Button
+    // HelloWorld,
+ 
   }
 }
 </script>
